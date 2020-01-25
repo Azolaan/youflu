6e3e95c9e3b0d6ae29cf99a75c21cab4877adf44
Alex <alo@evertz.com>
2020-01-25 15:58:57 -0500

    initialize mapbox

---
+++ src/map-component/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl'
+
+const Map = ReactMapboxGl({
+    accessToken: 'pk.eyJ1IjoibG9hIiwiYSI6ImNrNXUybW56NTE0emgzbW5zcmRldWM4YWoifQ.VOAMrWPyGzx1tpIeBbA8Zg'
+})
+
+class MapComponent extends React.Component {
+    render() {
+        return <Map
+            style="mapbox://styles/mapbox/dark-v10"
+            center={[-79.9209395, 43.2587083]}
+            containerStyle={{
+            height: '100vh',
+            width: '100vw'
+            }}
+        >
+            <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+                <Feature coordinates={[43.2587083, -79.9209395]} />
+            </Layer>
+        </Map>
+    }
+}
+
+export { MapComponent, MapComponent as default } 
