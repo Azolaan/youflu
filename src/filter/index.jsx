3c098ee3d48d9f58a228ac7c28f9a7223c433b43
Prachuyra Adhikary <deeptoadhikary@gmail.com>
2020-01-26 02:07:50 -0500

    user panel added

--- src/filter/index.jsx
+++ src/filter/index.jsx
@@ -1,42 +1,43 @@
-import React from 'react'
-import _ from 'lodash'
-import { Drawer, DrawerHeader, DrawerTitle, DrawerContent } from '@rmwc/drawer'
-import { List, SimpleListItem } from '@rmwc/list'
+import React from "react";
+import _ from "lodash";
+import { Drawer, DrawerHeader, DrawerTitle, DrawerContent } from "@rmwc/drawer";
+import { List, SimpleListItem } from "@rmwc/list";
 
-import diseases from './diseases'
+import diseases from "./diseases";
 
-import '@material/drawer/dist/mdc.drawer.css'
-import '@material/list/dist/mdc.list.css'
-
+import "@material/drawer/dist/mdc.drawer.css";
+import "@material/list/dist/mdc.list.css";
 
 class Filter extends React.Component {
-    _renderList = () => {
-        let diseaseList = _.map(diseases, (disease) => 
-                <SimpleListItem
-                    text={_.capitalize(disease)}
-                    activated={disease === this.props.filter}
-                    metaIcon={disease === this.props.filter && 'check'}
-                    onClick={() => this.props.setFilter(disease)}
-                />
-        )
+  _renderList = () => {
+    let diseaseList = _.map(diseases, disease => (
+      <SimpleListItem
+        text={_.capitalize(disease)}
+        activated={disease === this.props.filter}
+        metaIcon={disease === this.props.filter && "check"}
+        onClick={() => this.props.setFilter(disease)}
+      />
+    ));
 
-        return diseaseList
-    }
+    return diseaseList;
+  };
 
-    render() {
-        return (
-            <Drawer modal open={this.props.drawerOpen} onClose={this.props.onCloseDrawer}>
-                <DrawerHeader>
-                    <DrawerTitle>Immunity Filter</DrawerTitle>
-                </DrawerHeader>
-                <DrawerContent>
-                    <List>
-                        {this._renderList()}
-                    </List>
-                </DrawerContent>
-            </Drawer>
-        )
-    }
+  render() {
+    return (
+      <Drawer
+        modal
+        open={this.props.drawerOpen}
+        onClose={this.props.onCloseDrawer}
+      >
+        <DrawerHeader>
+          <DrawerTitle>Immunity Filter</DrawerTitle>
+        </DrawerHeader>
+        <DrawerContent>
+          <List>{this._renderList()}</List>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
 }
 
-export { Filter, Filter as default }
+export { Filter, Filter as default };
---
+++ src/user-panel-component/index.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import {
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  DialogButton
+} from "@rmwc/dialog";
+import { Drawer, DrawerTitle, DrawerHeader, DrawerContent } from "@rmwc/drawer";
+import { List, ListItem } from "@rmwc/list";
+import { Button } from "@rmwc/button";
+import { TextField } from "@rmwc/textfield";
+import { Checkbox } from "@rmwc/checkbox";
+import _ from "lodash";
+import diseases from "../filter/diseases";
+
+import "@material/dialog/dist/mdc.dialog.css";
+import "@material/button/dist/mdc.button.css";
+import "@material/textfield/dist/mdc.textfield.css";
+import "@material/checkbox/dist/mdc.checkbox.css";
+import "@material/form-field/dist/mdc.form-field.css";
+
+export default class UserPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openVaccDrawer: false
+    };
+  }
+
+  getDiseaseCheckbox = () => {
+    let diseaseCheckbox = _.map(diseases, disease => (
+      <Checkbox
+        label={_.capitalize(disease)}
+        style={{ width: "100%" }}
+      ></Checkbox>
+    ));
+
+    return diseaseCheckbox;
+  };
+
+  render() {
+    const { showUserPanel, toggleUserPanel } = this.props;
+    const { openVaccDrawer } = this.state;
+    return (
+      <form action="get">
+        <Dialog open={showUserPanel} onClose={toggleUserPanel}>
+          <DialogTitle>User Panel</DialogTitle>
+          <DialogContent>You can Modify your information.</DialogContent>
+          <TextField label="Postal Code" />
+
+          <div
+            className="disease-checkbox"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              overflowY: "scroll",
+              margin: "10px",
+              height: "300px"
+            }}
+          >
+            {this.getDiseaseCheckbox()}
+          </div>
+
+          <DialogActions>
+            <DialogButton type="submit" action="save">
+              Save
+            </DialogButton>
+            <DialogButton action="discard" isDefaultAction>
+              Discard
+            </DialogButton>
+          </DialogActions>
+        </Dialog>
+      </form>
+    );
+  }
+}
